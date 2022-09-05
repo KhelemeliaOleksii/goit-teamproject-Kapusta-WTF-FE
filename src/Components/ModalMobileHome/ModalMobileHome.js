b64a9f33853264b777a173040bf205d5ee46286b
ivanytskyi2407 <ivanytskyi2407@gmail.com>
2022-09-05 18:12:49 +0300

    +

--- src/Components/ModalMobileHome/ModalMobileHome.js
+++ src/Components/ModalMobileHome/ModalMobileHome.js
@@ -1,50 +1,50 @@
-import PropTypes from "prop-types";
-import { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import Button from "../Button";
-import s from "./ModalMobileHome.module.css";
-import transactionOperations from "../../redux/transaction/transaction-operations";
-import transactionSelectors from "../../redux/transaction/transaction-selectors";
-import { ReactComponent as Arrow } from "../../images/svg/arrow.svg";
-import { ReactComponent as Calculator } from "../../images/svg/calculator.svg";
+import PropTypes from 'prop-types';
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import Button from '../Button';
+import s from './ModalMobileHome.module.css';
+import transactionOperations from '../../redux/transaction/transaction-operations';
+import transactionSelectors from '../../redux/transaction/transaction-selectors';
+import { ReactComponent as Arrow } from '../../images/svg/arrow.svg';
+import { ReactComponent as Calculator } from '../../images/svg/calculator.svg';
 
 const options = [
-  { category: "expenses", value: "Transport", id: "63121fd1313da79b043d7b95" },
-  { category: "expenses", value: "Products", id: "63121fd1313da79b043d7b96" },
-  { category: "expenses", value: "Health", id: "63121fd1313da79b043d7b97" },
-  { category: "expenses", value: "Алкоголь", id: "63121fd1313da79b043d7b98" },
-  { category: "expenses", value: "Housing", id: "63121fd1313da79b043d7b9a" },
-  { category: "expenses", value: "Technique", id: "63121fd1313da79b043d7b9b" },
+  { category: 'expenses', value: 'Transport', id: '63121fd1313da79b043d7b95' },
+  { category: 'expenses', value: 'Products', id: '63121fd1313da79b043d7b96' },
+  { category: 'expenses', value: 'Health', id: '63121fd1313da79b043d7b97' },
+  { category: 'expenses', value: 'Алкоголь', id: '63121fd1313da79b043d7b98' },
+  { category: 'expenses', value: 'Housing', id: '63121fd1313da79b043d7b9a' },
+  { category: 'expenses', value: 'Technique', id: '63121fd1313da79b043d7b9b' },
   {
-    category: "expenses",
-    value: "Communal, communication",
-    id: "63121fd1313da79b043d7b9c",
+    category: 'expenses',
+    value: 'Communal, communication',
+    id: '63121fd1313da79b043d7b9c',
   },
   {
-    category: "expenses",
-    value: "Sports, hobbies",
-    id: "63121fd1313da79b043d7b9d",
+    category: 'expenses',
+    value: 'Sports, hobbies',
+    id: '63121fd1313da79b043d7b9d',
   },
-  { category: "expenses", value: "Education", id: "63121fd1313da79b043d7b9e" },
+  { category: 'expenses', value: 'Education', id: '63121fd1313da79b043d7b9e' },
   {
-    category: "expenses",
-    value: "Домашні улюбленці",
-    id: "63121fd1313da79b043d7b9f",
+    category: 'expenses',
+    value: 'Домашні улюбленці',
+    id: '63121fd1313da79b043d7b9f',
   },
   {
-    category: "expenses",
-    value: "Благодійність",
-    id: "63121fd1313da79b043d7ba0",
+    category: 'expenses',
+    value: 'Благодійність',
+    id: '63121fd1313da79b043d7ba0',
   },
-  { category: "expenses", value: "Інше", id: "63121fd1313da79b043d7ba1" },
-  { category: "income", value: "Salary", id: "63121fd1313da79b043d7ba2" },
-  { category: "income", value: 'Дод. дохід"', id: "63121fd1313da79b043d7ba3" },
+  { category: 'expenses', value: 'Інше', id: '63121fd1313da79b043d7ba1' },
+  { category: 'income', value: 'Salary', id: '63121fd1313da79b043d7ba2' },
+  { category: 'income', value: 'Дод. дохід"', id: '63121fd1313da79b043d7ba3' },
 ];
 
 function ModalExpenenses({ closeModal, category }) {
-  const [inputValue, setInputValue] = useState("");
-  const [selected, setSelected] = useState("");
-  const [InputMoney, setinputMoney] = useState("");
+  const [inputValue, setInputValue] = useState('');
+  const [selected, setSelected] = useState('');
+  const [InputMoney, setinputMoney] = useState('');
   const calendarDate = useSelector(transactionSelectors.getDate);
   const dispatch = useDispatch();
   const handleInputChange = (e) => {
@@ -58,9 +58,9 @@ function ModalExpenenses({ closeModal, category }) {
     setinputMoney(e.currentTarget.value);
   };
   const reset = () => {
-    setInputValue("");
-    setSelected("");
-    setinputMoney("");
+    setInputValue('');
+    setSelected('');
+    setinputMoney('');
   };
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -80,7 +80,7 @@ function ModalExpenenses({ closeModal, category }) {
     <div className={s.modalMobileHome}>
       <Arrow
         color="#FF751D"
-        style={{ position: "absolute", top: 22, left: 22 }}
+        style={{ position: 'absolute', top: 22, left: 22 }}
         onClick={closeModal}
       />
       <form onSubmit={handleSubmit} className={s.formMobileHome}>
@@ -124,7 +124,7 @@ function ModalExpenenses({ closeModal, category }) {
             <Button
               name="Input"
               type="submit"
-              style={{ background: "#FF751D", color: "#ffffff" }}
+              style={{ background: '#FF751D', color: '#ffffff' }}
             />
           </li>
           <li>
@@ -132,7 +132,7 @@ function ModalExpenenses({ closeModal, category }) {
               name="Clear"
               type="button"
               onClick={reset}
-              style={{ background: "##FFFFFF", color: "#52555F" }}
+              style={{ background: '##FFFFFF', color: '#52555F' }}
             />
           </li>
         </ul>
