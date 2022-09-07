1f5e96e3f9037570ab6db77a1447cd128c9545ef
KhelemeliaOleksii <xvdm@ukr.net>
2022-09-08 00:13:05 +0300

    try to fix

--- src/redux/balance/balance-operations.js
+++ src/redux/balance/balance-operations.js
@@ -3,21 +3,21 @@ const axios = require('axios');
 
 axios.defaults.baseURL = 'https://kapusta-wtf.herokuapp.com/';
 
-const getBalance = createAsyncThunk('balance/getBalance', async () => {
+const getBalance = createAsyncThunk('balance/getBalance', async (_, thunkAPI) => {
   try {
     const { data } = await axios.get('/api/v1/balance');
     return data;
   } catch (error) {
-    return error.message;
+    return thunkAPI.rejectWithValue(error.message);
   }
 });
 
-const addBalance = createAsyncThunk('balance/addBalance', async (balance) => {
+const addBalance = createAsyncThunk('balance/addBalance', async (balance, thunkAPI) => {
   try {
     const { data } = await axios.post('/api/v1/balance', balance);
     return data;
   } catch (error) {
-    return error.message;
+    return thunkAPI.rejectWithValue(error.message);
   }
 });
 const balanceOperations = {
