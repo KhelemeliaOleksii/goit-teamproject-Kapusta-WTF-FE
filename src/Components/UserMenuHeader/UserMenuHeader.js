6ded48bdf6d6ea65d7cd01c6502c57f98e41804c
Iryna S <irynaradkovska@gmail.com>
2022-09-14 10:25:03 +0300

    adds scroll to the MobileTable, animates the TeamModal

--- src/Components/UserMenuHeader/UserMenuHeader.js
+++ src/Components/UserMenuHeader/UserMenuHeader.js
@@ -1,5 +1,7 @@
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
+import styled, { keyframes } from 'styled-components';
+import { pulse } from 'react-animations';
 import LogoHeader from '../LogoHeader';
 import TeamModal from '../TeamModal';
 import UserLogOut from './UserLogOut/UserLogOut';
@@ -17,12 +19,14 @@ export default function UserMenuHeader() {
     setShowModal(false);
   };
 
+  const Pulse = styled.div`animation: 2s ${keyframes`${pulse}`} infinite`;
+
   return (
     <header className={styles.header}>
       <div className={styles.headerContainer}>
         <Link to="/" alt="homepage">
           <LogoHeader />
-          <button className={styles.button} type="button" onClick={openModal}>by WTF</button>
+          <Pulse><button className={styles.button} type="button" onClick={openModal}>by WTF</button></Pulse>
           {showModal && <TeamModal closeModal={closeModal} />}
         </Link>
         <UserLogOut />
