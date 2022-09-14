ef036f705d982db53703297b868fedee54ad40b3
Denys <kanedius@gmail.com>
2022-09-14 05:01:36 +0300

    fix mobile

--- src/Components/FormTransaction/FormTransaction.js
+++ src/Components/FormTransaction/FormTransaction.js
@@ -58,7 +58,7 @@ function FormTransaction({ category }) {
       return;
     }
     if (inputValue.length < 3 || inputValue.length > 20) {
-      toast.info('Поле повинно мiстити вiд 3 до 20 символiв', { theme: 'dark' });
+      toast.info('Поле опису повинно мiстити вiд 3 до 20 символiв', { theme: 'dark' });
       return;
     }
     if (InputMoney === '') {
